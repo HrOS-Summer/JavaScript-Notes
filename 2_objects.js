4fe1179b0af5008ae78991e410712a9e2bc20878
Hariom Singh <21bcs5872@cuchd.in>
2023-09-09 20:08:19 +0530

    object part1 completed

--- 2_objects.js
+++ 2_objects.js
@@ -3,7 +3,7 @@
 
 
 //symbol
-const mySym = Symbol("key1")
+const mySym = Symbol("key1") //symbol datatype
 
 //object literals - key value
 const JsUser = {
@@ -12,7 +12,12 @@ const JsUser = {
     0 : "Prez",
     isLoggedIn : false,
     lastLoginDays : ["Monday", "Friday"],
-    
+    //mySym : "myKey1"     //not a symbol anymore but a String
+}
+
+const JsUser2 = {
+    name : "Ravish",
+    [mySym] : "key2" //Syntax to add symbol to objects  
 }
 
 //in above object, name, isLoggedIn and lastLoginays are tracked as String
@@ -23,3 +28,21 @@ console.log(JsUser["lastLoginDays"]) //square notation
  
 //surname cannot be accessed by . operator as it is declared as a String
 console.log(JsUser["surname"])
+
+console.log(typeof mySym)
+console.log(typeof JsUser.mySym)
+console.log(JsUser2[mySym]) //symbol can only be accessed by square notation
+console.log(typeof JsUser2[mySym])
+
+//freeze object
+Object.freeze(JsUser2); //Now this object cannot be modified
+JsUser2.name = "Rathi"
+console.log(JsUser2)
+
+//function in object
+JsUser.greeting = function() {
+    console.log(`Hi ${this.name}`)  
+}
+
+console.log(JsUser.greeting) //here function is not executed just reference is passed
+console.log(JsUser.greeting()) //here function is passed
