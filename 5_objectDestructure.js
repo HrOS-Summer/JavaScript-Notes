21111a7799e42847f6dc620841872ceacc193b9c
Hariom Singh <21bcs5872@cuchd.in>
2023-09-09 23:09:49 +0530

    learnt about object destructuring and JSON

---
+++ 5_objectDestructure.js
@@ -0,0 +1,17 @@
+//Object Destructuring
+let course = {
+    courseInstructure : "Hitesh",
+    courseId : "213JBF"
+}
+
+const {courseInstructure : instructure} = course
+console.log(instructure)
+
+//JSON
+// {
+//     "name" : "Hiteh",
+//     "coursename" : "jsco1",
+//     "price" : "free"
+// }
+
+//API is also in form of JSON
