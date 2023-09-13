fc3d70e154539bf8a3dffcfa199bb529cc71d7c0
Hariom Singh <21bcs5872@cuchd.in>
2023-09-13 15:12:36 +0530

    learnt this in JavaScript

---
+++ 8_this.js
@@ -0,0 +1,21 @@
+const user = {
+    username : "hitesh",
+    price : 99,
+
+    welcomeMessage : function() {
+        console.log(`${this.username}, welcome back`)
+        console.log(this)
+    }
+}
+
+user.welcomeMessage()
+user.username = "Robin"
+user.welcomeMessage() //Robin 
+//If we do not use this then it will only work when username="hitesh"
+//But with the use of this it refers to ever current instance
+
+console.log(this) //In node environment, it will return empty object because
+//at this time there is no context in global
+
+//If we console.log(this) in browser then it will return window as it is global
+//object in browser
