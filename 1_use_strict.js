f35a93415c6efd406c0d0377577c1a9483dc5e7c
Hariom Singh <21bcs5872@cuchd.in>
2023-09-28 19:22:22 +0530

    updated 1

--- 1_use_strict.js
+++ 1_use_strict.js
@@ -1,74 +1,77 @@
- //"use strict"; // treat all code as newer version
+//"use strict"; // treat all code as newer version
  
 //In vs code we actually use Node.JS and in browser, JS
-const accountID = "21BCS5872" //cannot be reassigned or redeclared
-let accountNumber = "12130848" 
-var password = "Ggs@678" // reassigned, redeclared, global scope
+// const accountID = "21BCS5872" //cannot be reassigned or redeclared
+// let accountNumber = "12130848" 
+// var password = "Ggs@678" // reassigned, redeclared, global scope
 
-city = "Jaipur" //(not using let , var or const) does not work in strict mode
-// ^ datatype - any
+ //city = "Jaipur" //(not using let , var or const) does not work in strict mode
+ // ^ datatype - string
 //^global scope outside function and function scope if declared inside a function
-let state; //undefined
-let temp = null; //not undefined but contains no value
+// console.log(typeof city)
+// console.log(typeof(city))
 
-console.log(typeof city)
-console.log(typeof(city))
+ //let state; //undefined
+// let temp = null; //not undefined but contains no value
 
-console.table([accountID, accountNumber, password, city, state]);
+//const sur; //variables declared using const, must be initialized, else : SyntaxError
 
-//type conversion
+// console.table([accountID, accountNumber, password, city, state]);
 
-let score = "33"
-let scoreInNumber = Number(score)
-console.log(typeof score)
-console.log(typeof scoreInNumber)
+ //type conversion
+
+ //let score = "33"
+ //let scoreInNumber = Number(score)
+// console.log(typeof score)
+// console.log(typeof scoreInNumber)
+//console.log(scoreInNumber);
 
 //But
-let id = "123abc"
-let idNum = Number(id)
-console.log(typeof idNum); //Number
-console.log(idNum) //NaN - Not a number
+ //let id = "123abc"
+ //let idNum = Number(id)
+// console.log(typeof idNum); //Number
+// console.log(idNum) //NaN - Not a number
 
-//------To Boolean--------
-let myName = "" 
-let boo = Boolean(myName) //false
-console.log(boo);
+// //------To Boolean--------
+// let myName = "" 
+// let boo = Boolean(myName) //false
+// console.log(boo);
 
-let yourName = "Lk"
-let secondBoo = Boolean(yourName) //true
-console.log(secondBoo)
+// let yourName = "Lk"
+// let secondBoo = Boolean(yourName) //true
+// console.log(secondBoo)
 
-console.log("smtp")
-console.log("")
-console.log(+"") //converts string to boolean
+// console.log("smtp")
+// console.log("")
+// console.log(+"") //converts string to boolean
 
-let jh; //undefined
-let km = Boolean(jh);  //false
-console.log(km) 
+// let jh; //undefined
+// let km = Boolean(jh);  //false
+// console.log(km) 
 
-//----Number to String ------
-let num = 48
-let str = String (num)
-console.log(str)
-console.log(typeof str)
+// //----Number to String ------
+// let num = 48
+// let str = String (num)
+// console.log(str)
+// console.log(typeof str)
 
-// ---------Operations---------
-let val = 4
-let negVal = -val //storing -ve of a value
-console.log(negVal)
+// // ---------Operations---------
+// let val = 4
+// let negVal = -val //storing -ve of a value
+// console.log(negVal)
 
-console.log(1 + "1") //String
-console.log("1" + "1") //String
-console.log("1" + 3)  //String
-console.log("3" + 7 + 7) //String
+// console.log(1 + "1") //String
+// console.log("1" + "1") //String
+// console.log("1" + 3)  //String
+// console.log("3" + 7 + 7) //String
 
-console.log (1 + + "7") //Number
-console.log("2" + + "2") //String
-console.log("2" + + 2) //String
-console.log(3 + 2 + "6") // 56 Number, String
+// console.log (1 + + "7") //Number
+// console.log("2" + + "2") //String
+// console.log("2" + + 2) //String
+// console.log(3 + 2 + "6") // 56 Number, String
 
-console.log(4 - "1") //Number
-console.log("6" - "3") //Number
-console.log("5" - 4) //Number
+// console.log(4 - "1") //Number
+// console.log("6" - "3") //Number
+// console.log("5" - 4) //Number
 
-//A simple comment
+// //A simple comment
