493dd0d9c7847033ed4b19e9e683c824c015f46d
Hariom Singh <21bcs5872@cuchd.in>
2023-10-23 09:23:18 +0530

    Learnt DOM

---
+++ 18_DOM.js
@@ -0,0 +1,20 @@
+const parent = document.querySelector('.parent');
+parent.style.backgroundColor = "green";
+// console.log(parent);
+// console.log(parent.children); //HTML Collection
+// console.log(parent.children[0]);
+// console.log(parent.children[2].innerHTML);
+
+// for (let index = 0; index < parent.children.length; index++) {
+//     console.log(parent.children[index].innerHTML);    
+// }
+
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+// const dayOne = document.querySelector('.day');
+// console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling);
+
+console.log(parent.childNodes);
+//Node List counts every thing, the line break, commment, HTML elements etc.
